9dd938c79d4f1018dd0b3ab76b5b5e6b05914bf3
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-11-27 14:34:44 +0530

    [Shubham]Added: Demonstrated why local storage was local to account vs JSON server and removed dependency on employee class

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
-let empPayrollList;
+let employeePayrollList;
 
 window.addEventListener("DOMContentLoaded", (event)=> {
-    empPayrollList = getEmployeePayrollDataFromStrorage();
-    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    employeePayrollList = getEmployeePayrollDataFromStrorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
     localStorage.removeItem('editEmp');
 });
@@ -14,21 +14,21 @@ const getEmployeePayrollDataFromStrorage = () => {
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th> <th>Name</th>  <th>Gender</th> <th>Department</th> <th>Salary</th> <th>Start Date</th> <th>Actions</th>";
-    if(empPayrollList.length == 0) return;
+    if(employeePayrollList.length == 0) return;
     let innerHtml = `${headerHtml}`;
-    for(const empPayrollData of empPayrollList){
+    for(const employee of employeePayrollList){
     innerHtml = `${innerHtml}
 <tr>
-    <td><img class="profile" alt="" src="${empPayrollData._profilePic}" alt=""></td>
-    <td>${empPayrollData._name}</td>
-    <td>${empPayrollData._gender}</td>
-    <td><div class="dept-label">${getDeptHtml(empPayrollData._department)}</div>
-    <td>${empPayrollData._salary}</td>
-    <td>${stringifyDate(empPayrollData._startDate)}</td>
+    <td><img class="profile" alt="" src="${employee._profilePic}" alt=""></td>
+    <td>${employee._name}</td>
+    <td>${employee._gender}</td>
+    <td><div class="dept-label">${getDeptHtml(employee._department)}</div>
+    <td>${employee._salary}</td>
+    <td>${stringifyDate(employee._startDate)}</td>
     <td>
-        <img id = "${empPayrollData._name}" onclick="remove(this)" alt="delete"
+        <img id = "${employee.id}" onclick="remove(this)" alt="delete"
              src="../assets/assets/icons/delete-black-18dp.svg">
-        <img id ="${empPayrollData._name}" alt="edit" onclick="update(this)"
+        <img id ="${employee.id}" alt="edit" onclick="update(this)"
              src="../assets/assets/icons/create-black-18dp.svg">
     </td>
 </tr>
@@ -46,25 +46,25 @@ const getDeptHtml = (deptList) => {
 }
 
 const remove = (node) => {
-    let employee = empPayrollList.find(emp => emp._name == node.id);
+    let employee = employeePayrollList.find(emp => emp.id == node.id);
     if(!employee) return;
-    const index = empPayrollList.map(emp => emp._name)
-                                     .indexOf(employee._name);
-    empPayrollList.splice(index, 1);
-    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
-    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    const index = employeePayrollList.map(emp => emp.id)
+                                     .indexOf(employee.id);
+    employeePayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
 };
 
 const update = (node) => {
-    let employee = empPayrollList.find((emp) => emp._name == node.id);
+    let employee = employeePayrollList.find((emp) => emp.id == node.id);
     if (!employee) return;
     localStorage.setItem("editEmp", JSON.stringify(employee));
     window.location.replace(site_properties.add_emp_payroll_page);
   }; 
   
 const createEmployeePayrollJSON = () => {
-    let empPayrollListLocal = [
+    let employeePayrollListLocal = [
         {
             _name: 'Shubham Bhawsar',
             _gender: 'male',
@@ -72,7 +72,7 @@ const createEmployeePayrollJSON = () => {
             _salary: '50000',
             _startDate: '29 Oct 2019',
             _note: '',
-            _id: new Date().getTime(),
+            id: new Date().getTime(),
             _profilePic: '../assets/assets/profile-images/Ellipse -3.png'
         },
         {
@@ -82,9 +82,9 @@ const createEmployeePayrollJSON = () => {
             _salary: '50000',
             _startDate: '29 Oct 2019',
             _note: '',
-            _id: new Date().getTime() + 1,
+            id: new Date().getTime() + 1,
             _profilePic: '../assets/assets/profile-images/Ellipse -1.png'
         }
     ];
-    return empPayrollListLocal;
+    return employeePayrollListLocal;
 }
